decb4452a2d0d001b2c88025929431ac7efb4734
quickcoffee <31934316+quickcoffee@users.noreply.github.com>
2021-02-24 17:40:10 +0100

    adding source() to update_data.R

--- scripts/update_data.R
+++ scripts/update_data.R
@@ -1,2 +1,3 @@
+source("R/scrape_coronavirusupdate.R")
 scrape_coronavirusupdate(.all_episodes_url = "https://www.ndr.de/nachrichten/info/Coronavirus-Update-Alle-Folgen,podcastcoronavirus134.html",
                          .write_parquet = TRUE)
