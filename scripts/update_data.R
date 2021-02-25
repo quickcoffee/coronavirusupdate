2b0ef4344f6a6ab4990a87a617e982896561aaf2
Lukas Steger <lukas.steger95@gmail.com>
2021-02-25 13:26:49 +0100

    adding libary()s to update_data.R

--- scripts/update_data.R
+++ scripts/update_data.R
@@ -1,4 +1,16 @@
 library(magrittr)
+library(tibble)
+library(dplyr)
+library(lubridate)
+library(purrr)
+library(rvest)
+library(stringr)
+library(tidyr)
+library(urltools)
+library(xml2)
+library(arrow)
+
 source("R/scrape_coronavirusupdate.R")
+
 scrape_coronavirusupdate(.all_episodes_url = "https://www.ndr.de/nachrichten/info/Coronavirus-Update-Alle-Folgen,podcastcoronavirus134.html",
                          .write_parquet = TRUE)
